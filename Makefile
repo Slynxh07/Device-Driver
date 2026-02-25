75dc2ba34c2672ac9594027b6364e921763f04b7
Sean Lynch <seancrossbarry12@gmail.com>
2026-02-25 11:20:27 +0000

    Changed to makefile, way easier, prints to kernel Hello world when loaded

---
+++ Makefile
@@ -0,0 +1,15 @@
+obj-m += Driver.o
+
+Driver-objs := src/main.o
+
+KDIR := /lib/modules/$(shell uname -r)/build
+
+PWD := $(shell pwd)
+
+EXTRA_CFLAGS += -I$(PWD)/include
+
+all:
+	$(MAKE) -C $(KDIR) M=$(PWD) modules
+
+clean:
+	$(MAKE) -C $(KDIR) M=$(PWD) clean
--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
-#include <stdio.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
 
-int main()
-{
-    printf("Hello World\n");
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Sean");
+MODULE_DESCRIPTION("My first driver");
+
+static int __init driver_init(void) {
+    printk(KERN_INFO "Driver loaded\n");
+    return 0;
 }
+
+static void __exit driver_exit(void) {
+    printk(KERN_INFO "Driver unloaded\n");
+}
+
+module_init(driver_init);
+module_exit(driver_exit);
