0572d250560f4895036ebef3fc2d9557a8a1d109
Sean Lynch <seancrossbarry12@gmail.com>
2026-03-05 16:08:48 +0000

    Start of keyboard driver

--- src/main.c
+++ src/main.c
@@ -1,15 +1,60 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
 #include <linux/fs.h>
+#include <linux/usb.h>
+#include <linux/hid.h>
+
+#ifndef KBUILD_MODNAME
+#define KBUILD_MODNAME "keydriver"
+#endif
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Sean");
-MODULE_DESCRIPTION("My first driver");
+MODULE_DESCRIPTION("Keyboard Driver");
 
 int reg_dev(void);
 void dereg_dev(void);
 
+static struct usb_device_id keyboard_table[] = 
+{
+    { USB_INTERFACE_INFO(USB_INTERFACE_CLASS_HID, USB_INTERFACE_SUBCLASS_BOOT, USB_INTERFACE_PROTOCOL_KEYBOARD)},
+    {}
+};
+
+MODULE_DEVICE_TABLE(usb, keyboard_table);
+
+static int keyboard_probe(struct usb_interface *interface, const struct usb_device_id *id)
+{
+    printk(KERN_INFO "Keyboard connected\n");
+    return 0;
+}
+
+static void keyboard_disconnect(struct usb_interface *interface)
+{
+    printk(KERN_INFO "Keyboard disconnected\n");
+}
+
+//USB keyboards send 8-bytes HID reports every keypress
+//Example report
+/*
+Byte0: modifier
+Byte1: reserved
+Byte2-7: keycodes
+*/
+
+//Exaple 00 00 04 00 00 00 00 00
+// 04 = KEY_A
+
 //ssize_t device_file_read (struct file *, char *, size_t, loff_t *);
+
+static struct usb_driver keyboard_driver =
+{
+    .name = "myKeyboard",
+    .probe = keyboard_probe,
+    .disconnect = keyboard_disconnect,
+    .id_table = keyboard_table,
+};
 
 static struct file_operations simple_driver_fops = 
 {
@@ -18,11 +63,13 @@ static struct file_operations simple_driver_fops =
 
 int reg_dev(void)
 {
-    return register_chrdev(42, "My driver", &simple_driver_fops);
+    if (register_chrdev(42, "My driver", &simple_driver_fops)) return -1;
+    return usb_register(&keyboard_driver) < 0;
 }
 
 void dereg_dev(void)
 {
+    usb_deregister(&keyboard_driver);
     unregister_chrdev(42, "My driver");
 }
 
