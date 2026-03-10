0fef9226a7ec4868f79e9b7d312bcea6f3d7ca18
Sean Lynch <seancrossbarry12@gmail.com>
2026-03-10 16:05:20 +0000

    User program works when using sudo

--- user/src/user.c
+++ user/src/user.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
+
+#define KB_REPORT_SIZE 8
 
 int main()
 {
+    printf("starting program\n");
+
     int fd = open("/dev/keydriver0", O_RDONLY);
-    unsigned char key;
+
+    if (fd < 0)
+    {
+        perror("open failed");
+        return 1;
+    }
+
+    unsigned char report[KB_REPORT_SIZE];
 
     while (1)
     {
-        int n = read(fd, &key, 1);
-        if (n == 1)
-            printf("Keycode: %d\n", key);
+        read(fd, report, KB_REPORT_SIZE);
+        printf("Report: ");
+        for (int i = 0; i < KB_REPORT_SIZE; i++) printf("%02x ", report[i]);
+        printf("\n");
     }
 }
