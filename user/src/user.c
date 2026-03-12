bef3c511ab2b404b1a113916a517739c0a4badd9
Sean Lynch <seancrossbarry12@gmail.com>
2026-03-12 20:53:57 +0000

    Changed user program so it parses keycodes to string

--- user/src/user.c
+++ user/src/user.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
+#include "key_handler.h"
 
 #define KB_REPORT_SIZE 8
 
 int main()
 {
+    setbuf(stdout, NULL);
     printf("starting program\n");
 
     int fd = open("/dev/keydriver0", O_RDONLY);
@@ -14,7 +17,7 @@ int main()
     if (fd < 0)
     {
         perror("open failed");
-        return 1;
+        return -1;
     }
 
     unsigned char report[KB_REPORT_SIZE];
@@ -22,8 +25,14 @@ int main()
     while (1)
     {
         read(fd, report, KB_REPORT_SIZE);
-        printf("Report: ");
-        for (int i = 0; i < KB_REPORT_SIZE; i++) printf("%02x ", report[i]);
-        printf("\n");
+        //printf("Report: ");
+        //for (int i = 0; i < KB_REPORT_SIZE; i++) printf("%02x ", report[i]);
+        char *keys_string = get_keys(report, KB_REPORT_SIZE);
+        if (keys_string)
+        {
+            printf("%s", keys_string);
+            free(keys_string);
+        }
     }
+    printf("\n");
 }
